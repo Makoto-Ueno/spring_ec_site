9c0f98498b80ab711a71c4051188a2393aefa8ac
Makoto-Ueno <makoto991020@gmail.com>
2025-01-13 21:25:06 +0900

    init

---
+++ src/main/java/com/example/demo/SpringEcSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcSiteApplication.class, args);
+	}
+
+}
