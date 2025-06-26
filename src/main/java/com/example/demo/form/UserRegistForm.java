498837050aa8eb45ab8f8e77f28805723d36960b
moe <corpse.world@gmail.com>
2025-06-26 22:11:02 +0900

    passwordConfirmに@NotEmptyを追加

--- src/main/java/com/example/demo/form/UserRegistForm.java
+++ src/main/java/com/example/demo/form/UserRegistForm.java
@@ -2,7 +2,6 @@ package com.example.demo.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -13,11 +12,12 @@ public class UserRegistForm {
 	@Email(message = "メールアドレス形式で入力してください")
 	private String mail;
 
-	@NotNull(message = " パスワードを入力してください")
+	@NotEmpty(message = " パスワードを入力してください")
 	@Pattern(regexp = "^[a-zA-Z0-9'!\\-\"#$%&()*,./:;?@\\[\\]^_`{|}~+<=>]+$", message = "半角英数字・記号「'-!\"#$%&()*,./:;?@[]^_`{|}~+<=>」で入力してください")
 	@Size(min = 6, max = 16, message = "6文字以上16文字以下で入力してください")
 	private String password;
 
+	@NotEmpty(message = " パスワードを入力してください")
 	private String passwordConfirm;
 
 	public int getUserId() {
