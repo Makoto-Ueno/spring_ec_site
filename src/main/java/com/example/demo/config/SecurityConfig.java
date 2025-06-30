1fb568fd299383d1481f02410e16ef05c5d07126
moe <corpse.world@gmail.com>
2025-06-30 20:22:10 +0900

    JSファイルも許可するよう追加

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/css/**").permitAll() // CSSファイルは認証不要
+				.requestMatchers("/js/**").permitAll()// JSファイルは認証不要
 				.requestMatchers("/admin/**").permitAll() // 認証不要ぺージ設定 TODO：修正必要
 				.anyRequest().authenticated() // 他のURLはログイン後アクセス可能
 		).formLogin(login -> login.loginPage("/login").permitAll() // ログインページは認証不要
