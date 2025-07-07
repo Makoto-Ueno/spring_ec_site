fe0c35d98db7ba48b95b2e8419545882196dcca8
moe <corpse.world@gmail.com>
2025-07-07 22:41:17 +0900

    findByMailを修正

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,5 @@ import com.example.demo.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-	Object findByMail(String mail);
-
+	Optional<User> findByMail(String mail);
 }
