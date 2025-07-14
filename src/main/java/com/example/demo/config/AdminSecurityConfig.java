4c9f6a2bc25d7d8ff2b3b55cf344c7ce0944206b
moe <corpse.world@gmail.com>
2025-07-14 21:53:20 +0900

    管理者用SecurityConfig

---
+++ src/main/java/com/example/demo/config/AdminSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AdminSecurityConfig {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	@Qualifier("AdminDetailsServiceImpl")
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	@Order(1)
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.securityMatcher("/admin/**")
+				.authorizeHttpRequests(
+						authorize -> authorize.requestMatchers("/admin/**", "/css/**", "/js/**").hasRole("ADMIN"))
+				.formLogin(login -> login.loginPage("/admin/login") // ログインフォームのURL
+						.loginProcessingUrl("/admin/login") // フォームのPOST先
+						.defaultSuccessUrl("/admin", true) // ログイン成功時の遷移先
+						.failureUrl("/admin/login?error") // エラー時
+						.permitAll().usernameParameter("mail").passwordParameter("password"));
+		return http.build();
+	}
+
+}
