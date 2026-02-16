885974049f54c0775fc506a8a48be3940b1cacb9
moe <corpse.world@gmail.com>
2026-02-16 21:15:57 +0900

    ユーザー側ユーザー登録機能追加

---
+++ src/main/java/com/example/demo/controller/user/CustomerUserController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controller.user;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.entity.User;
+import com.example.demo.form.UserRegistForm;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+public class CustomerUserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@GetMapping("/register")
+	public String registerView(Model model, UserRegistForm form) {
+		model.addAttribute(form);
+		return "user/user/register";
+	}
+
+	@PostMapping("/register")
+	public String register(Model model, @ModelAttribute @Validated UserRegistForm form, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		// ▼ パスワードと確認用パスワードが一致しているかをチェック
+		if (!form.getPassword().equals(form.getPasswordConfirm())) {
+			// ▼ 一致しない場合、BindingResultにエラーを追加
+			bindingResult.rejectValue("passwordConfirm", "error.passwordConfirm", "パスワードと入力が一致しません");
+		}
+		// ▼ バリデーションエラーがある場合、登録画面に戻る
+		if (bindingResult.hasErrors()) {
+			model.addAttribute(form);
+			return "user/user/register";
+		}
+		// ▼ 追加: メールアドレスの重複チェック
+		if (!userRepository.findByMail(form.getMail()).isEmpty()) {
+			bindingResult.rejectValue("mail", "error.mail", "このメールアドレスは既に使用されています");
+			return "user/user/register";
+		}
+
+		User user = new User();
+		user.setMail(form.getMail());
+		// ハッシュ化
+		var hashPassword = passwordEncoder.encode(form.getPassword());
+		user.setPassword(hashPassword);
+		user.setType(0);
+		Date now = new Date();
+		user.setCreateAt(now);
+		user.setUpdateAt(now);
+
+		userRepository.saveAndFlush(user);
+		return "redirect:/";
+	}
+
+}
